c7e66084463d332e880fc793516a7560ea21f8df
sirfoxpoof <daniquedrost4@gmail.com>
2023-09-19 16:16:14 +0200

    Volume functionaliteit

--- TheCleanQueen/Assets/Scripts/UI&UX/VolumeSet.cs
+++ TheCleanQueen/Assets/Scripts/UI&UX/VolumeSet.cs
@@ -3,64 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using static UnityEngine.Rendering.DebugUI;
 
 public class VolumeSet : MonoBehaviour
 {
-    
     public AudioMixer mixer;
     public Slider masterVol, musicVol;
+    
 
-    private void Start()
+    public void Start()
     {
         if (PlayerPrefs.HasKey("MasterVolume"))
         {
+            
             mixer.SetFloat("MasterVolume", PlayerPrefs.GetFloat("MasterVolume"));
-            mixer.SetFloat("MusicVolume", PlayerPrefs.GetFloat("MusicVolume"));  
+            mixer.SetFloat("MusicVolume", PlayerPrefs.GetFloat("MusicVolume"));
             SetSliders();
         }
+        
         else
         {
             SetSliders();
         }
-    }
-
-    public void SetVolumeMaster(float sliderValue)
-    {
-        mixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
 
     }
 
-    public void SetVolumeMusic(float sliderValue)
-    {
-        mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
-
-    }
-
-    public void SetSliders()
+    void SetSliders()
     {
         masterVol.value = PlayerPrefs.GetFloat("MasterVolume");
         musicVol.value = PlayerPrefs.GetFloat("MusicVolume");
-
     }
 
-    public void UpdateMasterVol()
-    {
-        mixer.SetFloat("MasterVolume", masterVol.value);
+
+    public void SetVolumeMaster(float sliderValue)
+     {
+        mixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
         PlayerPrefs.SetFloat("MasterVolume", masterVol.value);
-    }
+     }
 
-    public void UpdateMusicVol()
-    {
-        mixer.SetFloat("MusicVolume", musicVol.value);
+     public void SetVolumeMusic(float sliderValue)
+     {
+        mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
         PlayerPrefs.SetFloat("MusicVolume", musicVol.value);
-        
-    }
+     }
 
-    public void SetFullscreen(bool isFullscreen)
-    {
-        Screen.fullScreen = isFullscreen;
-        print("fullscreen toggle");
-    }
+
+     public void SetFullscreen(bool isFullscreen)
+     {
+         Screen.fullScreen = isFullscreen;
+         print("fullscreen toggle");
+     }
+    
+  
 
 
 }
