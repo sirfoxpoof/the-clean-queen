55877c1559bada735bd108d16699ba59f7e8cfc7
sirfoxpoof <daniquedrost4@gmail.com>
2023-10-29 23:12:30 +0100

    Level select aangepast

---
+++ TheCleanQueen/Assets/Scripts/UI&UX/DetectingClick.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DetectingClick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField]
+    private GameObject popup;
+    public void OnPointerDown(PointerEventData pointerEventData)
+    {
+        popup.SetActive(false);
+        print("wazaa");
+
+    }
+
+    
+    public void OnPointerUp(PointerEventData pointerEventData)
+    {
+        //heheheheheh
+    }
+}
